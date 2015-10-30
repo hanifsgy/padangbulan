7f548de453d3c8f013404987c6c2654398d8609a
hanifsgy@gmail.com <sugiyanto011>
2015-10-30 15:03:39 +0700

    asu mumet

--- app/src/main/java/com/example/sikikan/pb01/MainActivity.java
+++ app/src/main/java/com/example/sikikan/pb01/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sikikan.pb01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,4 +22,36 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // if action bar click, action bar show
+        // automatically handle clicks
+        // parent di AndroidManifest.xml
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.add) {
+
+            startActivity(new Intent(this, ActivityAmbilPeran.class));
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+
+    //search bar
+
+
+
+
+
+
+    }
+
